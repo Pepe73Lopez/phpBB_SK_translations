dfb442df7c889d04757d34e51e02be6335754eee
Pepe73Lopez <Pepe73Lopez@users.noreply.github.com>
2015-02-20 16:36:54 +0100

    Create search_ignore_words.php

---
+++ search_ignore_words.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* @package language
+* @copyright (c) 2007 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+$words = array(
+	'a',
+	'o',
+	'po',
+	'pred',
+	'aby',
+	'že',
+	'keď',
+	'ale',
+	'lebo',
+	'aj',
+	'ani',
+	'na',
+	'popri',
+);
