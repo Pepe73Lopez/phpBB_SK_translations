cc90088e93af73342d2182a1eb0418cf422122e8
Tomáš <fonziset@gmail.com>
2015-02-27 16:31:55 +0100

    Update viewtopic.php

--- sk/viewtopic.php
+++ sk/viewtopic.php
@@ -38,92 +38,92 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'APPROVE'				       => 'Súhlasím',
+	'APPROVE'				               => 'Súhlasím',
 	'ATTACHMENT'			               => 'Príloha',
-	'ATTACHMENT_FUNCTIONALITY_DISABLED'	       => 'Možnosť vkladať prílohy nie je povolená.',
+	'ATTACHMENT_FUNCTIONALITY_DISABLED'	   => 'Možnosť vkladať prílohy nie je povolená.',
 
 	'BOOKMARK_ADDED'		               => 'Téma bola pridaná medzi záložky.',
 	'BOOKMARK_ERR'			               => 'Pridanie témy medzi záložky zlyhalo. Skúste znovu.',
 	'BOOKMARK_REMOVED'		               => 'Téma bola odstránená zo záložiek.',
 	'BOOKMARK_TOPIC'		               => 'Pridať tému medzi záložky',
-	'BOOKMARK_TOPIC_REMOVE'	                       => 'Odstrániť zo záložiek',
-	'BUMPED_BY'			               => 'Naposledy oživil %1$s - %2$s.',
+	'BOOKMARK_TOPIC_REMOVE'	               => 'Odstrániť zo záložiek',
+	'BUMPED_BY'			                   => 'Naposledy oživil %1$s - %2$s.',
 	'BUMP_TOPIC'			               => 'Oživiť tému',
 
-	'CODE'				               => 'Kód',
+	'CODE'				                   => 'Kód',
 
-	'DELETE_TOPIC'		                       => 'Vymazať tému',
-	'DELETED_INFORMATION'                          => 'Vymazal %1$s on %2$s',
-	'DISAPPROVE'		                       => 'Nesúhlasím',
+	'DELETE_TOPIC'		                   => 'Vymazať tému',
+	'DELETED_INFORMATION'                  => 'Vymazal %1$s on %2$s',
+	'DISAPPROVE'		                   => 'Nesúhlasím',
 	'DOWNLOAD_NOTICE'		               => 'Na prezeranie priložených súborov nemáte dostatočné oprávnenia.',
 
-	'EDITED_TIMES_TOTAL'	                       => array(
+	'EDITED_TIMES_TOTAL'	               => array(
 		1	=> 'Naposledy upravil/-a %2$s v %3$s, upravené celkom %1$d krát.',
-                2	=> 'Naposledy upravil/-a %2$s v %3$s, upravené celkom %1$d krát.',
-                3	=> 'Naposledy upravil/-a %2$s v %3$s, upravené celkom %1$d krát.',
+        2	=> 'Naposledy upravil/-a %2$s v %3$s, upravené celkom %1$d krát.',
+        3	=> 'Naposledy upravil/-a %2$s v %3$s, upravené celkom %1$d krát.',
 	),
 	'EMAIL_TOPIC'			=> 'Odoslať tému emailom',
-	'ERROR_NO_ATTACHMENT'	        => 'Zvolená príloha už neexistuje.',
+	'ERROR_NO_ATTACHMENT'	=> 'Zvolená príloha už neexistuje.',
 
-	'FILE_NOT_FOUND_404'     	=> 'Súbor <strong>%s</strong> neexistuje.',
+	'FILE_NOT_FOUND_404'    => 'Súbor <strong>%s</strong> neexistuje.',
 	'FORK_TOPIC'			=> 'Kopírovať tému',
 	'FULL_EDITOR'			=> 'Úplný Editor&amp;Náhľad',
 
 	'LINKAGE_FORBIDDEN'		=> 'Nemáte oprávnenia prezerať túto stránku, sťahovať z nej alebo používať odkazy na nej uvedené.',
-	'LOGIN_NOTIFY_TOPIC'	        => 'Boli ste upozornený na túto tému prosím prihláste sa, aby ste si ju mohli prezrieť.',
+	'LOGIN_NOTIFY_TOPIC'	=> 'Boli ste upozornený na túto tému prosím prihláste sa, aby ste si ju mohli prezrieť.',
 	'LOGIN_VIEWTOPIC'		=> 'Na prezeranie tejto témy je potrebné byť registrovaný a prihlásený.',
 
-	'MAKE_ANNOUNCE'	         	=> 'Zmeniť na “Oznámenie”',
+	'MAKE_ANNOUNCE'	        => 'Zmeniť na “Oznámenie”',
 	'MAKE_GLOBAL'			=> 'Zmeniť na “Oznámenie pre celý portál”',
 	'MAKE_NORMAL'			=> 'Zmeniť na “Obyčajnú tému”',
 	'MAKE_STICKY'			=> 'Pripnúť tému na začiatok zoznamu tém.',
-	'MAX_OPTIONS_SELECT'	        => array(
-                1	=> 'Môžete označiť <strong>%d</strong> možnosť',
+	'MAX_OPTIONS_SELECT'    => array(
+        1	=> 'Môžete označiť <strong>%d</strong> možnosť',
 	 	2	=> 'Môžete označiť <strong>%d</strong> možnosti',
-                3       => 'Môžete označiť až <strong>%d</strong> možností',
+        3   => 'Môžete označiť až <strong>%d</strong> možností',
 	),
 	'MISSING_INLINE_ATTACHMENT'	=> 'Príloha <strong>%s</strong> už nie je dosupná',
-	'MOVE_TOPIC'			=> 'Presunúť tému',
+	'MOVE_TOPIC'			    => 'Presunúť tému',
 
-	'NO_ATTACHMENT_SELECTED'         => 'Nezvolili ste žiadnu prílohu.',
+	'NO_ATTACHMENT_SELECTED' => 'Nezvolili ste žiadnu prílohu.',
 	'NO_NEWER_TOPICS'		 => 'Neexistujú novšie témy.',
 	'NO_OLDER_TOPICS'		 => 'Neexistujú staršie témy.',
 	'NO_UNREAD_POSTS'		 => 'V tejto téme nie sú žiadne nové neprečítané príspevky.',
 	'NO_VOTE_OPTION'		 => 'Musíte vybrať jednu z možností, ak chcete hlasovať.',
-	'NO_VOTES'			 => 'Žiadne hlasovania',
+	'NO_VOTES'			     => 'Žiadne hlasovania',
 
-	'POLL_ENDED_AT'	          	 => 'Hlasovanie ukončené %s',
-	'POLL_RUN_TILL'	             	 => 'Hlasovanie spustené až do %s',
+	'POLL_ENDED_AT'	         => 'Hlasovanie ukončené %s',
+	'POLL_RUN_TILL'	         => 'Hlasovanie spustené až do %s',
 	'POLL_VOTED_OPTION'		 => 'Za túto možnosť ste už hlasovali',
-	'POST_DELETED_RESTORE'	         => 'Tento príspevok bol vymazaný. Môže byť obnovený.',
+	'POST_DELETED_RESTORE'   => 'Tento príspevok bol vymazaný. Môže byť obnovený.',
 	'PRINT_TOPIC'			 => 'Náhľad pre tlač',
 
 	'QUICK_MOD'			 => 'Rýchle nástroje',
-	'QUICKREPLY'			 => 'Rýchla odpoveď',
+	'QUICKREPLY'		 => 'Rýchla odpoveď',
 	'QUOTE'				 => 'Citovať príspevok',
 
-	'REPLY_TO_TOPIC'		 => 'Odpovedať k tejto téme',
+	'REPLY_TO_TOPIC'	 => 'Odpovedať k tejto téme',
 	'RESTORE'			 => 'Obnoviť',
-	'RESTORE_TOPIC'		         => 'Obnoviť tému',
-	'RETURN_POST'			 => '%sNávrat na príspevok%s',
+	'RESTORE_TOPIC'	     => 'Obnoviť tému',
+	'RETURN_POST'		 => '%sNávrat na príspevok%s',
 
-	'SUBMIT_VOTE'			 => 'Hlasovať',
+	'SUBMIT_VOTE'		 => 'Hlasovať',
 
-	'TOPIC_TOOLS'		 	 => 'Nástroje pre tému',
-	'TOTAL_VOTES'			 => 'Celkom hlasov',
+	'TOPIC_TOOLS'		 => 'Nástroje pre tému',
+	'TOTAL_VOTES'		 => 'Celkom hlasov',
 
-	'UNLOCK_TOPIC'			 => 'Odomknúť tému',
+	'UNLOCK_TOPIC'		 => 'Odomknúť tému',
 
 	'VIEW_INFO'			 => 'Detaily príspevku',
-	'VIEW_NEXT_TOPIC'		 => 'Ďalší príspevok',
-	'VIEW_PREVIOUS_TOPIC'	         => 'Predchádzajúci príspevok',
-	'VIEW_RESULTS'			 => 'Zobraziť výsledky',
-	'VIEW_TOPIC_POSTS'		 => array(
+	'VIEW_NEXT_TOPIC'	 => 'Ďalší príspevok',
+	'VIEW_PREVIOUS_TOPIC'=> 'Predchádzajúci príspevok',
+	'VIEW_RESULTS'		 => 'Zobraziť výsledky',
+	'VIEW_TOPIC_POSTS'	 => array(
 		1	=> '%d príspevok',
 		2	=> '%d príspevky',
-                3	=> '%d príspevkov',
+        3	=> '%d príspevkov',
 	),
-	'VIEW_UNREAD_POST'		=> 'Prvý neprečítaný príspevok',
-	'VOTE_SUBMITTED'		=> 'Váš hlas bol započítaný.',
-	'VOTE_CONVERTED'		=> 'Nie je možné zmeniť váše hlasovanie.',
+	'VIEW_UNREAD_POST'	=> 'Prvý neprečítaný príspevok',
+	'VOTE_SUBMITTED'	=> 'Váš hlas bol započítaný.',
+	'VOTE_CONVERTED'	=> 'Nie je možné zmeniť váše hlasovanie.',
 ));
