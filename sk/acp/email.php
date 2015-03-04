fd30f2a639023e6f646b26f1011e44989064cb61
Tomáš <fonziset@gmail.com>
2015-03-04 20:56:25 +0100

    Update email.php

--- sk/acp/email.php
+++ sk/acp/email.php
@@ -1,16 +1,11 @@
 <?php
 /**
 *
-* This file is part of the phpBB Forum Software package.
-*
-* @copyright (c) phpBB Limited <https://www.phpbb.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
+* @package language
+* @copyright (c) 2007 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
-
 
 /**
 * DO NOT CHANGE
