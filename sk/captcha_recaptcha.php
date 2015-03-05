d3ce5d09c4cdeda5a37fd103a784d5a2ab46c278
Tomáš <fonziset@gmail.com>
2015-03-05 18:40:13 +0100

    Update captcha_recaptcha.php

--- sk/captcha_recaptcha.php
+++ sk/captcha_recaptcha.php
@@ -2,6 +2,7 @@
 /**
 *
 * @package language
+* @authors PepeLopez, Fonzi
 * @copyright (c) 2007 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
@@ -33,16 +34,16 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'RECAPTCHA_LANG' => 'sk',
-	'RECAPTCHA_NOT_AVAILABLE' => 'Ak chcete použiť reCaptcha, musíte si vytvoriť účet na <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
-	'CAPTCHA_RECAPTCHA' => 'reCaptcha',
-	'RECAPTCHA_INCORRECT' => 'Zadaný overovací kód je nesprávny',
+	'RECAPTCHA_LANG'			=> 'sk',
+	'RECAPTCHA_NOT_AVAILABLE'	=> 'Ak chcete použiť reCaptcha, musíte si vytvoriť účet na <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'CAPTCHA_RECAPTCHA'			=> 'reCaptcha',
+	'RECAPTCHA_INCORRECT'		=> 'Zadaný overovací kód je nesprávny',
 
-	'RECAPTCHA_PUBLIC' => 'Verejný reCaptcha kľúč',
-	'RECAPTCHA_PUBLIC_EXPLAIN' => 'Váš verejný reCaptcha kľúč. Kľúče možno získať na <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
-	'RECAPTCHA_PRIVATE'	=> 'Súkromný reCaptcha kľúč',
+	'RECAPTCHA_PUBLIC'			=> 'Verejný reCaptcha kľúč',
+	'RECAPTCHA_PUBLIC_EXPLAIN' 	=> 'Váš verejný reCaptcha kľúč. Kľúče možno získať na <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'RECAPTCHA_PRIVATE'			=> 'Súkromný reCaptcha kľúč',
 	'RECAPTCHA_PRIVATE_EXPLAIN'	=> 'Váš súkromný reCaptcha kľúč. Kľúče možno získať na <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
 
-	'RECAPTCHA_EXPLAIN'	=> 'Snažíme sa zamedziť automatizovaným registráciám, preto je potrebné, aby ste do rámčekov vpísali obe slová.',
-	'RECAPTCHA_SOCKET_ERROR' => 'Problém s pripojením na službu RECAPTCHA: nemožné vytvoriť spojenie. Skúste znovu.',
+	'RECAPTCHA_EXPLAIN'			=> 'Snažíme sa zamedziť automatizovaným registráciám, preto je potrebné, aby ste do rámčekov vpísali obe slová.',
+	'RECAPTCHA_SOCKET_ERROR'	=> 'Problém s pripojením na službu RECAPTCHA: nemožné vytvoriť spojenie. Skúste znovu.',
 ));
