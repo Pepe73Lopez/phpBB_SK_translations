0f3c495a041e12fad82cc7d4d6a37119f85065c2
Tomáš <fonziset@gmail.com>
2015-03-12 19:25:32 +0100

    Update groups.php

--- sk/acp/groups.php
+++ sk/acp/groups.php
@@ -1,6 +1,9 @@
 <?php
 /**
 *
+*
+* @authors PepeLopez, Fonzi
+*
 * @package language
 * @copyright (c) 2007 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
